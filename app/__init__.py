056297b98cbcf541894216eaee06f0dc4e10251a
Mike Presman <mikepresman@gmail.com>
2021-02-20 15:36:56 -0500

    API

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,14 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from flask_mail import Mail
 from flask_wtf.csrf import CSRFProtect
-
+from flask_cors import CORS
 import os
 
 
 app = Flask(__name__)
+
+CORS(app)
 
 app.config.from_object(Config) #this inherits the Config class, makes the Flask config be overwritten by this
 
@@ -19,10 +20,6 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 login = LoginManager(app)
-
-mail = Mail(app)
-
-csrf = CSRFProtect(app)
 
 
 
--- app/routes.py
+++ app/routes.py
@@ -2,17 +2,41 @@ from app import app, db
 from config import Config
 from app.models import User
 
-from flask import render_template, redirect, flash, url_for, request, session
+from flask import render_template, redirect, flash, url_for, request, session, jsonify
 from flask_login import current_user, login_user, logout_user, login_required
 
+from werkzeug.security import generate_password_hash, check_password_hash
 
-@app.route("/", methods=["GET"])
-def index():
-    user = User.query.all()
-    return render_template("index.html", user = user)
+#dummy account: username a, password a
+
+@app.route('/register', methods = ["POST"])
+def register():
+    username = request.json["username"]
+    password = request.json["password"]
 
 
-@app.route("/bad", methods = ["GET", "POST"])
-def bad():
-    return render_template("bad.html")
-    
+    if (User.query.filter_by(username = username).first() == None):
+        hashed_pword = generate_password_hash(password)
+        user = User(username = username, password_hash = hashed_pword)
+        db.session.add(user)
+        db.session.commit()
+        return {'Success': True}
+    return {'Success': False}
+
+
+@app.route("/login", methods = ["POST"])
+def login():
+    username = request.json["username"]
+    password = request.json["password"]
+    if (User.query.filter_by(username = username).first() != None):
+        u = User.query.filter_by(username = username).first()
+        if (check_password_hash(u.password_hash, password) == True):
+            login_user(u)
+            return {'Login': 'Success'}  
+
+        
+    return {"GotIt": True}
+
+@app.route("/hello")
+def hello():
+    return {"Hello": 1, "Time": "Now"}
